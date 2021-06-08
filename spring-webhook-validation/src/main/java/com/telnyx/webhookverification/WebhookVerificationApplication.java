3e6db0cae06c1692b7f7ed0ae5c628e7e46c2809
Drew Kitch <drew@telnyx.com>
2021-06-08 10:48:10 -0600

    spring webhook validation

---
+++ spring-webhook-validation/src/main/java/com/telnyx/webhookverification/WebhookVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.telnyx.webhookverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookVerificationApplication.class, args);
+	}
+
+}
